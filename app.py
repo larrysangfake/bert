9f1e3ba7931a9d6d5583e879148289d4d739a430
larrysangfake <larry.sang@student-cs.fr>
2024-06-08 21:53:08 +0200

    add stopwords to phrase clouds

--- app.py
+++ app.py
@@ -744,7 +744,7 @@ if dashboard == 'Section 4: Learning':
     bigram_freq = Counter(bigrams)
 
     # Generate the word cloud
-    phrase_cloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(bigram_freq)
+    phrase_cloud = WordCloud(width=800, height=400, background_color='white', stopwords=learning_stopwords).generate_from_frequencies(bigram_freq)
 
     # Display the word cloud using Streamlit
     st.title('Phrase Cloud')
