24543937a742b666538f1978173c3d113b4d797c
larrysangfake <156656452+larrysangfake@users.noreply.github.com>
2024-06-08 15:35:04 +0200

    Update app.py pipeline

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import pipeline
 import torch
 import pandas as pd
 import numpy as np
